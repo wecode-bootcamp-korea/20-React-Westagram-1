37f72044fb588c1d8aa8da029e8d70d5c44790f1
하연주 <llilyha123@gmail.com>
2021-04-29 16:07:13 +0900

    modify img, icon

--- src/pages/Yeonju/Main/Components/Stories/Stories.js
+++ src/pages/Yeonju/Main/Components/Stories/Stories.js
@@ -12,7 +12,7 @@ class Stories extends React.Component {
             <li>
               <div className="gradient-wrap">
                 <img
-                  src="https://scontent-ssn1-1.cdninstagram.com/v/t51.2885-19/s150x150/35986726_903848573151322_399536158729568256_n.jpg?tp=1&_nc_ht=scontent-ssn1-1.cdninstagram.com&_nc_ohc=oGlfWXPE-bcAX9atN7M&edm=ABfd0MgAAAAA&ccb=7-4&oh=f611f5be4a0ab8559d5e52a25ec6441e&oe=60A9E126&_nc_sid=7bff83"
+                  src="http://localhost:3000/images/Yeonju/yeonju.jpg"
                   alt="나의 프로필 사진"
                 />
               </div>
@@ -20,7 +20,7 @@ class Stories extends React.Component {
             <div className="gradient-wrap">
               <li>
                 <img
-                  src="http://file3.instiz.net/data/cached_img/upload/2020/12/13/12/25b0643c8fe29ff2248fcf03c1b44106.jpg"
+                  src="http://localhost:3000/images/Yeonju/taeyeon.jpg"
                   alt="태연 프로필 사진"
                 />
               </li>
@@ -28,7 +28,7 @@ class Stories extends React.Component {
             <div className="gradient-wrap">
               <li>
                 <img
-                  src="https://dispatch.cdnser.be/wp-content/uploads/2018/03/2da3a9715f273c35f60a038061679457.png"
+                  src="http://localhost:3000/images/Yeonju/IU.png"
                   alt="아이유 프로필 사진"
                 />
               </li>
@@ -36,7 +36,7 @@ class Stories extends React.Component {
             <div className="gradient-wrap">
               <li>
                 <img
-                  src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2F0rfwn%2Fbtq2niMNw5P%2F3G3gG7dt8YUYgEV6xxf4f0%2Fimg.png"
+                  src="http://localhost:3000/images/Yeonju/haery.png"
                   alt="이해리 프로필 사진"
                 />
               </li>
@@ -44,7 +44,7 @@ class Stories extends React.Component {
             <div className="gradient-wrap">
               <li>
                 <img
-                  src="https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FotT26%2Fbtq2wLTL1P0%2Fn8fzhXS3TbSHYncSk29tv1%2Fimg.png"
+                  src="http://localhost:3000/images/Yeonju/minkyung.png"
                   alt="강민경 프로필 사진"
                 />
               </li>
