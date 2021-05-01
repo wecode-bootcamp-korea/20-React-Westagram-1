80d60b103f48b73d76c5e08761a3664ae9269853
하연주 <llilyha123@gmail.com>
2021-05-01 15:41:50 +0900

    Add : Mission 1 - 사용자 입력 데이터 저장 기능 구현

--- src/pages/Yeonju/Login/LoginYeonju.js
+++ src/pages/Yeonju/Login/LoginYeonju.js
@@ -3,8 +3,27 @@ import './LoginYeonju.scss';
 import { withRouter } from 'react-router-dom';
 
 class LoginYeonju extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      idValue: '',
+      pwValue: '',
+    };
+  }
   goToMain = () => {
     this.props.history.push('/mainyeonju');
+  };
+
+  handleIdInput = e => {
+    this.setState({
+      idValue: e.target.value,
+    });
+  };
+
+  handlePwInput = e => {
+    this.setState({
+      pwValue: e.target.value,
+    });
   };
 
   render() {
@@ -12,13 +31,20 @@ class LoginYeonju extends React.Component {
       <div className="LoginYeonju">
         <div className="outbox">
           <header className="westagram"> Westagram </header>
+
           <form className="id_pw">
             <input
               className="idInput"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
+              onChange={this.handleIdInput}
             />
-            <input className="pwInput" type="password" placeholder="비밀번호" />
+            <input
+              className="pwInput"
+              type="password"
+              placeholder="비밀번호"
+              onChange={this.handlePwInput}
+            />
             <button type="button" onClick={this.goToMain}>
               로그인
             </button>
