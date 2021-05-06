09a309381c6b73d026b946f99341e46e6192fc3d
Kim Yeseul <96linda@hanmail.net>
2021-05-06 20:15:29 +0900

    Add : 회원가입, 로그인 기능 구현

--- src/pages/Yeseul/Login/Login.js
+++ src/pages/Yeseul/Login/Login.js
@@ -8,6 +8,7 @@ class LoginYeseul extends Component {
     this.state = {
       inputId: '',
       inputPw: '',
+      loginMode: true,
     };
   }
 
@@ -16,18 +17,76 @@ class LoginYeseul extends Component {
     this.setState({ [name]: value });
   };
 
-  goToMain = () => {
-    this.props.history.push('/main-yeseul');
+  convertMode = () => {
+    this.setState({
+      loginMode: !this.state.loginMode,
+    });
+  };
+
+  signIn = e => {
+    const { inputId, inputPw } = this.state;
+    e.preventDefault();
+    fetch('http://10.58.6.252:8000/users/signin', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: inputId,
+        password: inputPw,
+      }),
+    })
+      .then(users => users.json())
+      .then(users => {
+        if (users.MESSAGE === 'SUCCESS') {
+          this.setState({
+            inputId: '',
+            inputPw: '',
+          });
+          localStorage.setItem('token', users.ACCESS_TOKEN);
+          this.props.history.push('/main-yeseul');
+        } else if (users.MESSAGE === 'INVALID_USER') {
+          const wantToSignUp = window.confirm(
+            '잘못된 정보입니다. 회원가입하시겠습니까?'
+          );
+          wantToSignUp && this.convertMode();
+        }
+      });
+  };
+
+  signUp = e => {
+    const { inputId, inputPw } = this.state;
+    e.preventDefault();
+    fetch('http://10.58.6.252:8000/users/signup', {
+      method: 'POST',
+      body: JSON.stringify({
+        email: inputId,
+        password: inputPw,
+      }),
+    })
+      .then(users => users.json())
+      .then(users => {
+        if (users.MESSAGE === 'SUCCESS') {
+          this.setState({
+            inputId: '',
+            inputPw: '',
+          });
+          alert(`회원가입되었습니다! 🎉<br/>로그인해주세요`);
+          this.convertMode();
+        } else {
+          alert(users.MESSAGE);
+        }
+      });
   };
 
   render() {
-    const { inputId, inputPw } = this.state;
+    const { inputId, inputPw, loginMode } = this.state;
     const checkId = /^\w[\w\-.]*@\w+\.\w{2,}/;
 
     return (
       <main className="loginYeseul give-border">
         <h1 className="logo">westagram</h1>
-        <form className="login-form">
+        <form
+          className="login-form"
+          onSubmit={loginMode ? this.signIn : this.signUp}
+        >
           <div className="login-form__input-box">
             <input
               type="text"
@@ -45,11 +104,10 @@ class LoginYeseul extends Component {
             />
           </div>
           <button
-            type="button"
-            onClick={this.goToMain}
-            disabled={!(checkId.test(inputId) && inputPw.length > 5)}
+            type="submit"
+            disabled={!(checkId.test(inputId) && inputPw.length > 8)}
           >
-            로그인
+            {loginMode ? '로그인' : '회원가입'}
           </button>
         </form>
         <a
